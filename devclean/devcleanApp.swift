4f723b810eb0e1e86731cf57c067cfd3cc5c6770
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2026-03-12 22:53:09 +0800

    Initial Commit

---
+++ devclean/devcleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  devcleanApp.swift
+//  devclean
+//
+//  Created by Ahmad Sufyan on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct devcleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
